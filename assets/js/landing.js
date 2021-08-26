204d701ccdbd0cdf9abc2e1a3e551b188a827320
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-08-26 15:59:24 +0300

    Removing landing page + assets

--- assets/js/landing.js
+++
@@ -1,9 +0,0 @@
-const fadeZoom = () => {
-    setTimeout(() => {
-        $(".content")
-            .css("opacity", "1")
-            .css("transform", "scale(1)");
-    }, 250);
-};
-
-window.onload = fadeZoom();
