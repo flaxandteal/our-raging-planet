718b02c32de03b9b9074075fcd7fa902cb983ef2
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-10-30 22:40:12 +0300

    Commenting out section scroll handler

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,63 +40,63 @@ const menuScrollHandler = () => {
 
 // SECTION SCROLL BEHAVIOUR
 
-const sectionScrollHandler = () => {
-    const scroll = $(window).scrollTop();
-    const menuHeight = parseInt($("nav").css("height"));
+// const sectionScrollHandler = () => {
+//     const scroll = $(window).scrollTop();
+//     const menuHeight = parseInt($("nav").css("height"));
 
-    const about = $("#about");
-    const features = $("#features");
-    const featuresList = $("#features-list");
-    const resources = $("#resources");
+//     const about = $("#about");
+//     const features = $("#features");
+//     const featuresList = $("#features-list");
+//     const resources = $("#resources");
 
-    if (about.length === 0 && features.length === 0 && featuresList.length === 0 && resources.length === 0) return;
+//     if (about.length === 0 && features.length === 0 && featuresList.length === 0 && resources.length === 0) return;
 
-    if (about.visible()) {
-        $("#about .content")
-            .css("opacity", "1")
-            .css("transform", "scale(1)");
-    } else if (scroll >= about.offset().top - menuHeight) {
-        $("#about .content")
-            .css("opacity", "1")
-            .css("transform", "scale(1)");
-    }
+//     if (about.visible()) {
+//         $("#about .content")
+//             .css("opacity", "1")
+//             .css("transform", "scale(1)");
+//     } else if (scroll >= about.offset().top - menuHeight) {
+//         $("#about .content")
+//             .css("opacity", "1")
+//             .css("transform", "scale(1)");
+//     }
 
-    if (features.visible()) {
-        $("#features .content")
-            .css("opacity", "1")
-            .css("transform", "scale(1)");
-    } else if (scroll >= features.offset().top - menuHeight) {
-        $("#features .content")
-            .css("opacity", "1")
-            .css("transform", "scale(1)");
-    }
+//     if (features.visible()) {
+//         $("#features .content")
+//             .css("opacity", "1")
+//             .css("transform", "scale(1)");
+//     } else if (scroll >= features.offset().top - menuHeight) {
+//         $("#features .content")
+//             .css("opacity", "1")
+//             .css("transform", "scale(1)");
+//     }
 
-    if (featuresList.visible()) {
-        $("#features-list .content")
-            .css("opacity", "1")
-            .css("transform", "scale(1)");
-    } else if (scroll >= featuresList.offset().top - menuHeight) {
-        $("#features-list .content")
-            .css("opacity", "1")
-            .css("transform", "scale(1)");
-    }
+//     if (featuresList.visible()) {
+//         $("#features-list .content")
+//             .css("opacity", "1")
+//             .css("transform", "scale(1)");
+//     } else if (scroll >= featuresList.offset().top - menuHeight) {
+//         $("#features-list .content")
+//             .css("opacity", "1")
+//             .css("transform", "scale(1)");
+//     }
 
-    if (resources.visible()) {
-        $("#resources .content")
-            .css("opacity", "1")
-            .css("transform", "scale(1)");
-    } else if (scroll >= resources.offset().top - menuHeight) {
-        $("#resources .content")
-            .css("opacity", "1")
-            .css("transform", "scale(1)");
-    }
-}
+//     if (resources.visible()) {
+//         $("#resources .content")
+//             .css("opacity", "1")
+//             .css("transform", "scale(1)");
+//     } else if (scroll >= resources.offset().top - menuHeight) {
+//         $("#resources .content")
+//             .css("opacity", "1")
+//             .css("transform", "scale(1)");
+//     }
+// }
 
 // CALLING SCROLL HANDLERS
 
 const scrollHandlers = () => {
     menuScrollHandler();
-    sectionScrollHandler();
+    // sectionScrollHandler();
 }
 
 $(window).on("scroll", scrollHandlers);
